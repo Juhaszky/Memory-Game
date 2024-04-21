cfbaa7df946e7cfa2039e4a3ab98341fa8b08ee2
Juhaszky <juhasz.krisztian2000@gmail.com>
2024-04-21 17:22:25 +0200

    feat(settings): settings slice provided

--- src/store.js
+++ src/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import counterSlice from './features/counter/counterSlice'
+import settingsSlice from './features/settings/settingsSlice'
 
 export default configureStore({
   reducer: {
-    counter: counterSlice
+    counter: counterSlice,
+    settings: settingsSlice
   }
 })
