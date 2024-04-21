941a1b56acb8b422af6889c3755d891adcfe1a2c
Juhaszky <juhasz.krisztian2000@gmail.com>
2024-04-21 22:37:39 +0200

    refact(card): flipping a card logic

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,10 +1,31 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { flipCard, resetFlips } from "../../features/game/gameSlice";
 
-const Card = () => {
+const Card = ({ cardNumber }) => {
+    const dispatch = useDispatch();
+    const flippedCards = useSelector(state => state.game.flippedCards);
+
+    const isSelected = flippedCards.includes(cardNumber);
+    const twoSelected = flippedCards.length === 2;
+
+    const selectCard = () => {
+        if (flippedCards.includes(cardNumber)) return;
+        const willBeSecondFlip = flippedCards.length === 1;
+
+        dispatch(flipCard(cardNumber));
+
+        if (willBeSecondFlip) {
+            setTimeout(() => {
+                dispatch(resetFlips());
+            }, 1000);
+        }
+        console.log(`selected card: ${cardNumber}`);
+    }
     return (
 
-        <div className="card sm:w-52 w-40 sm:h-52 h-40 bg-slate-400 p-3 rounded-md shadow-lg">
-            <p>Card Title</p>
+        <div onClick={selectCard} className={`card flex w-full justify-items items-center sm:w-52 w-24 sm:h-52 h-40 bg-slate-400 p-3 rounded-md shadow-lg ${isSelected ? 'bg-red-200' : ''} ${twoSelected ? 'pointer-events-none' : ''}`}>
+            <p>Card Title - {cardNumber}</p>
         </div>
     )
 }
