09d2b3dfee54bc59fc4b59a476aabbf80246e1b7
Juhaszky <juhasz.krisztian2000@gmail.com>
2024-04-21 22:37:58 +0200

    refact(settings): reset flips on level change

--- src/components/settings-layout/inputSettings/settings-layout.jsx
+++ src/components/settings-layout/inputSettings/settings-layout.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useDispatch, useSelector, } from "react-redux";
 import { setLevel } from "../../../features/settings/settingsSlice";
+import { resetFlips } from "../../../features/game/gameSlice";
 
 const SettingsLayout = () => {
     const dispatch = useDispatch();
@@ -10,11 +11,11 @@ const SettingsLayout = () => {
         const newLevel = event.target.value;
 
         dispatch(setLevel(newLevel));
-        console.log(level);
+        dispatch(resetFlips());
     }
     return (
-        <section className="settings-layout flex justify-center">
-            <select onChange={handleChange}>
+        <section className="settings-layout flex justify-center ">
+            <select className='bg-slate-500 text-slate-100' onChange={handleChange}>
                 <option value={0}>Level - 1 (4x4)</option>
                 <option value={1}>Level - 2 (6x6)</option>
                 <option value={2}>Level - 3 (8x8)</option>
